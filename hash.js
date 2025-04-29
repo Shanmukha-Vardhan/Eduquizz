834c8cfd863fc447aa9cf3f0f335c480ac8e6f7a
Shanmukha-Vardhan <ssalapu@gitam.in>
2025-04-29 16:51:35 +0530

    Add Test Click button and ensure event binding in StudentDashboard in Phase 2

--- hash.js
+++
@@ -1,8 +0,0 @@
-const bcrypt = require('bcrypt');
-const password = 'admin123';
-const saltRounds = 10;
-bcrypt.hash(password, saltRounds, (err, hash) => {
-    if (err) throw err;
-    console.log(hash);
-    process.exit();
-});
